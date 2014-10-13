7fe290f87ef3394a3383c5cd0923cab3e543879d
tiriksan <kristian.aabrekk@gmail.com>
2014-10-13 21:31:40 +0200

    Setup

---
+++ Assets/Scripts/CarAI.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarAI : MonoBehaviour {
+
+    public Transform startRoad;
+    public Transform endRoad;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody.velocity = new Vector3(-speed, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(transform.position.x < endRoad.position.x)
+        {
+            transform.position = startRoad.position;
+        }
+        rigidbody.velocity = new Vector3(-speed, 0, 0);
+	}
+}
