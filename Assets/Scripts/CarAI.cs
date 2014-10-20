b5f0c87e37e83a55a3b51624f69d7a441323a877
Chris Nord <msnorc@gmail.com>
2014-10-20 15:07:21 +0200

    nobackToday

--- Assets/Scripts/CarAI.cs
+++ Assets/Scripts/CarAI.cs
@@ -1,60 +1,71 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CarAI : MonoBehaviour {
+public class CarAI : MonoBehaviour
+{
 
-    public Transform startRoad;
-    public Transform endRoad;
-    public float speed;
-	public float deAccelSpeed = 1;
-	public float accelSpeed = 1;
-    public bool insideBox;
+		public Transform startRoad;
+		public Transform endRoad;
+		public float speed;
+		public float deAccelSpeed = 1;
+		public float accelSpeed = 1;
+		public bool insideBox;
+		public bool insideCarField;
 
-	// Use this for initialization
-	void Start () 
-	{
-        rigidbody.velocity = new Vector3(speed * transform.forward.x, 0, 0);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if(transform.position.x < endRoad.position.x)
-        {
-            transform.position = startRoad.position;
-        }
-        if(rigidbody.velocity.magnitude < speed && !insideBox)
-        {
-            accel();
-        }
-        //rigidbody.velocity = new Vector3(-speed, 0, 0);
-	}
-
-	//deAccel
-	public void deAccel()
-	{
-		//for (int i = 0; i > speed; i++) {
-		if (rigidbody.velocity.x * transform.forward.x > 0) 
-		{	
-			rigidbody.velocity -= new Vector3 (deAccelSpeed * transform.forward.x, 0, 0);
-		}	
-	
-		//}
-	}
-
-	//accel
-	public void accel()
-	{
-        if(rigidbody.velocity.x * transform.forward.x < speed)
+		// Use this for initialization
+		void Start ()
 		{
-			rigidbody.velocity += new Vector3(accelSpeed * transform.forward.x, 0,0);
+				rigidbody.velocity = new Vector3 (speed * transform.forward.x, 0, 0);
 		}
-	}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				if (transform.position.x < endRoad.position.x) {
+						transform.position = startRoad.position;
+				}
+				if (rigidbody.velocity.magnitude < speed && !insideBox && !insideCarField) {
+						accel ();
+				}
+				//rigidbody.velocity = new Vector3(-speed, 0, 0);
+		}
 
-    void OnCollisionEnter (Collision col)
-    {
-        if(col.collider.tag == "Player")
-        {
-            //Player dies...
-        }
-    }
+		//deAccel
+		public void deAccel ()
+		{
+
+				if (rigidbody.velocity.x * transform.forward.x > 0) {	
+						rigidbody.velocity -= new Vector3 (deAccelSpeed * transform.forward.x, 0, 0);
+				}
+		if((rigidbody.velocity.x * transform.forward.x < 0))
+		{
+			rigidbody.velocity = Vector3.zero;
+		}
+	
+				//}
+		}
+
+		//accel
+		public void accel ()
+		{
+				if (rigidbody.velocity.x * transform.forward.x < speed) {
+						rigidbody.velocity += new Vector3 (accelSpeed * transform.forward.x, 0, 0);
+				}
+		}
+
+		//field in front of car
+		public void OnTriggerStay (Collider col)
+		{
+				if (col.tag == "Player" || col.tag == "Car") {
+						Debug.Log (rigidbody.velocity.magnitude);
+						deAccel ();
+				}
+		}
+
+		void OnCollisionEnter (Collision col)
+		{
+				if (col.collider.tag == "Player") {
+						//Player dies...
+				}
+		}
 }
