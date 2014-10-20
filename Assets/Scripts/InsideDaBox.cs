4f9abef97251f518aba10bed580a49f37d0f6d95
tiriksan <kristian.aabrekk@gmail.com>
2014-10-20 12:06:50 +0200

    InsideDaBox

---
+++ Assets/Scripts/InsideDaBox.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InsideDaBox : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerStay (Collider col)
+    {
+        if(col.tag == "Player")
+        {
+            //TODO if car driving towards player
+
+            //TODO all car objects kinda
+            GameObject.Find("CC_ME_R4").GetComponent<CarAI>().deAccel();
+        }
+    }
+}
