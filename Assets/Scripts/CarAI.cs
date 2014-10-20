10863ea25d5abf709bfadb822fc91cbcf7970d4c
Chris Nord <msnorc@gmail.com>
2014-10-20 14:21:19 +0200

    accel

--- Assets/Scripts/CarAI.cs
+++ Assets/Scripts/CarAI.cs
@@ -7,9 +7,11 @@ public class CarAI : MonoBehaviour {
     public Transform endRoad;
     public float speed;
 	public float deAccelSpeed = 1;
+	public float accelSpeed = 1;
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
         rigidbody.velocity = new Vector3(-speed, 0, 0);
 	}
 	
@@ -26,11 +28,21 @@ public class CarAI : MonoBehaviour {
 	public void deAccel()
 	{
 		//for (int i = 0; i > speed; i++) {
-		if (rigidbody.velocity.x < 0) {	
+		if (rigidbody.velocity.x < 0) 
+		{	
 			rigidbody.velocity += new Vector3 (deAccelSpeed, 0, 0);
 		}	
 	
 		//}
+	}
+
+	//accel
+	public void accel()
+	{
+		if (rigidbody.velocity.x < speed * transform.forward.x)
+		{
+			rigidbody.velocity -= new Vector3(accelSpeed * transform.forward.x, 0,0);
+		}
 	}
 
     void OnCollisionEnter (Collision col)
