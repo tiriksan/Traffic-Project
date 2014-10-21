f73a4b33f6377e83264b58cb0547d91e610eb82f
Chris Nord <msnorc@gmail.com>
2014-10-21 16:27:59 +0200

    ch ch ch ch changes

--- Assets/Scripts/TrafficLights.cs
+++ Assets/Scripts/TrafficLights.cs
@@ -23,6 +23,7 @@ public class TrafficLights : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(timer);
         timer += Time.deltaTime;
         if (timer > offsetTime)
         {
