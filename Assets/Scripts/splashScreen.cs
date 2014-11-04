c23f0210e91bda3f320896bc7c595725988b35d4
Chris Nord <msnorc@gmail.com>
2014-11-04 15:13:59 +0100

    gogo

---
+++ Assets/Scripts/splashScreen.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class splashScreen : MonoBehaviour {
+
+	public static bool dead;
+	private int deaths;
+	private int score;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI(){
+	
+	}
+}
