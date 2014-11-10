a95d56501ebf7933e9752dc2fb161264600ae6c8
tiriksan <kristian.aabrekk@gmail.com>
2014-11-10 16:53:53 +0100

    awesome particle stuff

---
+++ Assets/Scripts/VictoryTrigger.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VictoryTrigger : MonoBehaviour {
+    void OnTriggerEnter(Collider col)
+    {
+        GameObject.Find("Stats").GetComponent<splashScreen>().victory = true;
+        Debug.Log("adsfjsdlfkjsalødkfjaslødkfj");
+    }
+    void OnTriggerStay(Collider col)
+    {
+        col.rigidbody.AddForce(Vector3.up*20);
+    }
+}
