62dba449d30a7ef6a8e35a533774ab272ec6f6ef
Chris Nord <msnorc@gmail.com>
2014-11-10 15:19:36 +0100

    allAboutThoseStats

--- Assets/Scripts/splashScreen.cs
+++ Assets/Scripts/splashScreen.cs
@@ -4,25 +4,56 @@ using System.Collections;
 public class splashScreen : MonoBehaviour {
 
 	public Transform stats;
+	public Transform screen;
 	public static bool dead;
-	private int deaths;
+	private static int deaths = 1;
 	private int score;
+
+	//public static Transform[] allChildren = GetComponentsInChildren<Transform>();
+
+	void Awake(){
+		Debug.Log("start " + deaths);
+		DontDestroyOnLoad(this);
+	}
 
 	// Use this for initialization
 	void Start () {
-	
+
+		score = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(dead){
-			Debug.Log("you're dead, huehue");
-			this.renderer.enabled = true;
-			//this.enabled = true;
+		this.renderer.enabled = false;
+		//gameObject.GetComponentInChildren<MeshRenderer>().enabled = true;
 
+		if(dead){
+
+			Debug.Log("deaths : " + deaths);
+			this.renderer.enabled = true;
+			screen.gameObject.SetActive(true);
+
+			this.GetComponent<TextMesh>().text = "You're injured!\nyou've been hit\n " + deaths
+				+ " times\n wait to respawn..";
+
+			Invoke("Respawn",3);
+		}
+	}
+
+	void Respawn(){
+		if(dead){
+			dead = false;
+			//Application.LoadLevelAdditive(0);
+			deaths++;
+			Debug.Log ("death" + deaths);
+			Application.LoadLevel(0);
 
 		}
 	}
+
+
+
+
 
 
 
