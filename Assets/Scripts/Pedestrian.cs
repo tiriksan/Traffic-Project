cf1fce25df2255fee68bbbadff7c2b31477e8a33
Chris Nord <msnorc@gmail.com>
2014-11-11 17:28:01 +0100

    bajbaj

---
+++ Assets/Scripts/Pedestrian.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pedestrian : MonoBehaviour {
+
+	public TriggerField triggerField;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay(Collider col){
+		if(col.tag == "Player"){
+			triggerField.isActive = true;
+		}
+	}
+
+	void OnTriggerExit(Collider col){
+		if(col.tag == "Player"){
+			triggerField.isActive = false;
+		}
+	}
+}
